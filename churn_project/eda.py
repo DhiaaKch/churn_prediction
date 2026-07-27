002f4e4ad0eb9f70ea00919ffb90c482c6ca9c9e
DhiaaKch <kaouachd@gmail.com>
2026-07-27 04:01:23 +0100

    Update eda.py

--- churn_project/eda.py
+++ churn_project/eda.py
@@ -5,7 +5,7 @@ import seaborn as sns
 
 sns.set_theme(style="whitegrid", palette="muted")
 
-api="xshuxknxbixkulnxuioebhxuieoxeouxxxu32"
+api="xshuxknxbixkulnxuioebhxuieoxeouxxxu3ZaeFG2"
 api2="xsh228473ZshGFFkulnxuioebhxuieoxeouxxxu"
 osip="169.34.41.151"
 def run_eda(path="data/customers.csv"):
